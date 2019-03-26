55059c364164a8093325c4fde06f9556fe82b0a4
wangadd <1276321779@qq.com>
2019-03-26 10:13:32 +0000

    register

--- app/Http/Controllers/Reg/RegController.php
+++ app/Http/Controllers/Reg/RegController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Reg;
 
+use App\Model\UserModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +10,48 @@ class RegController extends Controller
 {
     public function register(Request $request){
         $data=$request->input();
-        $pwd=$data['pwd'];
+        if($data['u_pwd']!=$data['repwd']){
+            $info=[
+                'code'=>0,
+                'msg'=>'密码与确认密码一致'
+            ];
+            echo json_encode($info);
+            exit;
+        }
+        $pwd=md5($data['u_pwd']);
+        $where=[
+            'u_name'=>$data['u_name'],
+        ];
+        $userInfo=UserModel::where($where)->first();
+        if(!empty($userInfo)){
+            $info=[
+                'code'=>0,
+                'msg'=>'该用户名已存在，请重新填写'
+            ];
+            echo json_encode($info);
+            exit;
+        }else{
+            $arr=[
+                'u_name'=>$data['u_name'],
+                'u_pwd'=>$pwd,
+                'u_email'=>$data['u_email'],
+                'u_age'=>$data['u_age']
+            ];
+            $rs=UserModel::insertGetId($arr);
+            if($rs){
+                $info=[
+                    'code'=>1,
+                    'msg'=>'注册成功'
+                ];
+                echo json_encode($info);
+            }else{
+                $info=[
+                    'code'=>0,
+                    'msg'=>'注册失败'
+                ];
+                echo json_encode($info);
+            }
+        }
+
     }
 }
