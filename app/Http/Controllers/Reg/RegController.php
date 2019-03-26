86131e3fdb2a790618fe8066597450cbbb8562a2
wangadd <1276321779@qq.com>
2019-03-26 08:19:51 +0000

    register

---
+++ app/Http/Controllers/Reg/RegController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Reg;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RegController extends Controller
+{
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/register',"Reg\RegController@register");
